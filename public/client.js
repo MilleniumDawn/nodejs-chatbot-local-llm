49c1703bf9659d9409342e26903cbc3ab5d7b44a
MilleniumDawn <122926088+MilleniumDawn@users.noreply.github.com>
2023-10-24 15:26:50 -0400

    ES6 + prettier

--- public/client.js
+++ public/client.js
@@ -1,32 +1,34 @@
+// client.js
 import { io } from "https://cdn.skypack.dev/socket.io-client@4";
 
 const socket = io();
 
 let botMessageDiv; // Declare a variable to hold the current bot message div
 
-window.onload = () => {
-    function sendMessage() {
-        const message = document.getElementById("input").value;
-        const messagesDiv = document.getElementById("messages");
-        messagesDiv.innerHTML += `<div class="user">You: ${message}</div>`;
-        botMessageDiv = document.createElement("div"); // Create a new div for the bot message
-        botMessageDiv.className = "bot"; // Set the class name of the bot message div
-        messagesDiv.appendChild(botMessageDiv); // Append the bot message div to the messages div
-        socket.emit("user message", message);
-        document.getElementById("input").value = "";
+function sendMessage() {
+    const message = document.getElementById("input").value;
+    const messagesDiv = document.getElementById("messages");
+    messagesDiv.innerHTML += `<div class="user">You: ${message}</div>`;
+    botMessageDiv = document.createElement("div"); // Create a new div for the bot message
+    botMessageDiv.className = "bot"; // Set the class name of the bot message div
+    messagesDiv.appendChild(botMessageDiv); // Append the bot message div to the messages div
+    socket.emit("user message", message);
+    document.getElementById("input").value = "";
+}
+
+document.getElementById("input").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault(); // Prevents the default action of the enter key
+        sendMessage();
     }
+});
 
-    document.getElementById("input").addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            e.preventDefault(); // Prevents the default action of the enter key
-            sendMessage();
-        }
-    });
+socket.on("bot message", (msg) => {
+    botMessageDiv.innerHTML += msg; // Append the chunk of text to the current bot message div
+    const messagesDiv = document.getElementById("messages");
+    messagesDiv.scrollTop = messagesDiv.scrollHeight; // Auto-scroll to bottom
+});
 
-    socket.on("bot message", (msg) => {
-        botMessageDiv.innerHTML += msg; // Append the chunk of text to the current bot message div
-        const messagesDiv = document.getElementById("messages");
-        messagesDiv.scrollTop = messagesDiv.scrollHeight; // Auto-scroll to bottom
-    });
-window.sendMessage = sendMessage; // Expose the function to the global scope
-};
+export function handleSendButtonClick() {
+    sendMessage();
+}
