5eb5cff7c6e042f6697f7257eea0f1438a6f7a50
MilleniumDawn <122926088+MilleniumDawn@users.noreply.github.com>
2023-10-24 11:35:58 -0400

    work with local api

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// Import required libraries
-import axios from 'axios';
-import readlineSync from 'readline-sync';
-import colors from 'colors';
+// Import the necessary modules
+import axios from "axios";
+import readlineSync from "readline-sync";
+import colors from "colors";
 
-// Define the main function
 async function main() {
     console.log(colors.bold.green("Welcome to the Chatbot Program!"));
     console.log(colors.bold.green("You can start chatting with the bot."));
 
-    const chatHistory = [];  // Store conversation history
+    const chatHistory = []; // Store conversation history
 
     while (true) {
         const userInput = readlineSync.question(colors.yellow("You: "));
@@ -23,19 +22,22 @@ async function main() {
             // Add latest user input
             messages.push({ role: "user", content: userInput });
 
-            // Define the API request data
-            const requestData = {
-                messages,
-                stop: ["### Instruction:"],
-                temperature: 0.7,
-                max_tokens: -1,
-                stream: false
-            };
-
-            // Send a request to the local API
-            const response = await axios.post('http://localhost:1234/v1/chat/completions', requestData, {
-                headers: { 'Content-Type': 'application/json' },
-            });
+            // Call the API with user input & history
+            const response = await axios.post(
+                "http://localhost:1234/v1/chat/completions",
+                {
+                    messages: messages,
+                    stop: ["### Instruction:"],
+                    temperature: 0.7,
+                    max_tokens: -1,
+                    stream: false,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
 
             // Get completion text/content
             const completionText = response.data.choices[0].message.content;
@@ -56,5 +58,4 @@ async function main() {
     }
 }
 
-// Call the main function
 main();
