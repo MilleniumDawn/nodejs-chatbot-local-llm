faa318527ed699eb336e37056ea9afa724437eb8
MilleniumDawn <122926088+MilleniumDawn@users.noreply.github.com>
2023-10-24 15:06:56 -0400

    working, made pretty

--- public/client.js
+++ public/client.js
@@ -1,28 +1,32 @@
-import { io } from 'https://cdn.skypack.dev/socket.io-client@4';
+import { io } from "https://cdn.skypack.dev/socket.io-client@4";
 
 const socket = io();
 
-let botMessageDiv;  // Declare a variable to hold the current bot message div
+let botMessageDiv; // Declare a variable to hold the current bot message div
 
-function sendMessage() {
-    const message = document.getElementById('input').value;
-    const messagesDiv = document.getElementById('messages');
-    messagesDiv.innerHTML += `<div>You: ${message}</div>`;
-    botMessageDiv = document.createElement('div');  // Create a new div for the bot message
-    messagesDiv.appendChild(botMessageDiv);  // Append the bot message div to the messages div
-    socket.emit('user message', message);
-    document.getElementById('input').value = '';
-}
-
-socket.on('bot message', (msg) => {
-    botMessageDiv.innerHTML += msg;  // Append the chunk of text to the current bot message div
-});
-
-document.getElementById('input').addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-        sendMessage();
+window.onload = () => {
+    function sendMessage() {
+        const message = document.getElementById("input").value;
+        const messagesDiv = document.getElementById("messages");
+        messagesDiv.innerHTML += `<div class="user">You: ${message}</div>`;
+        botMessageDiv = document.createElement("div"); // Create a new div for the bot message
+        botMessageDiv.className = "bot"; // Set the class name of the bot message div
+        messagesDiv.appendChild(botMessageDiv); // Append the bot message div to the messages div
+        socket.emit("user message", message);
+        document.getElementById("input").value = "";
     }
-});
 
+    document.getElementById("input").addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Prevents the default action of the enter key
+            sendMessage();
+        }
+    });
 
-window.sendMessage = sendMessage;  // Expose the function to the global scope
+    socket.on("bot message", (msg) => {
+        botMessageDiv.innerHTML += msg; // Append the chunk of text to the current bot message div
+        const messagesDiv = document.getElementById("messages");
+        messagesDiv.scrollTop = messagesDiv.scrollHeight; // Auto-scroll to bottom
+    });
+window.sendMessage = sendMessage; // Expose the function to the global scope
+};
