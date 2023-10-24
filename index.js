c12757a80dea3a6be1051261839b24f578528845
MilleniumDawn <122926088+MilleniumDawn@users.noreply.github.com>
2023-10-24 13:49:06 -0400

    Update index.js
    streaming work and formated

--- index.js
+++ index.js
@@ -8,77 +8,69 @@ async function main() {
     console.log(colors.bold.green("Welcome to the Chatbot Program!"));
     console.log(colors.bold.green("You can start chatting with the bot."));
 
-    const chatHistory = []; // Store conversation history
-
     while (true) {
         const userInput = readlineSync.question(colors.yellow("You: "));
+        let isFirstChunk = true;  // Add this line
 
-        try {
-            // Construct messages by iterating over the history
-            const messages = chatHistory.map(([role, content]) => ({
-                role,
-                content,
-            }));
+        await new Promise(async (resolve, reject) => {
+            try {
+                const messages = [{ role: "user", content: userInput }];
 
-            // Add latest user input
-            messages.push({ role: "user", content: userInput });
+                const axiosInstance = axios.create({
+                    baseURL: "http://localhost:1234/v1",
+                    headers: { "Content-Type": "application/json" },
+                    responseType: "stream",
+                });
 
-            // Create an axios instance for streaming
-            const axiosInstance = axios.create({
-                baseURL: "http://localhost:1234/v1",
-                headers: { "Content-Type": "application/json" },
-                responseType: 'stream'
-            });
-
-            // Call the API with user input & history
-            const response = await axiosInstance.post(
-                "/chat/completions",
-                {
+                const response = await axiosInstance.post("/chat/completions", {
                     messages: messages,
                     stop: ["### Instruction:"],
                     temperature: 0.7,
                     max_tokens: -1,
                     stream: true,
-                }
-            );
+                });
 
-            // The response data should now be a readable stream
-            const stream = response.data;
-            
-            stream.on('data', (chunk) => {
-                const payloads = chunk.toString().split("\n\n");
-                for (const payload of payloads) {
-                    if (payload.includes('[DONE]')) return;
-                    if (payload.startsWith("data:")) {
-                        const data = JSON.parse(payload.replace("data: ", ""));
+                const stream = response.data;
+
+                stream.on("data", (chunk) => {
+                    const lines = chunk
+                        .toString()
+                        .split("\n")
+                        .filter((line) => line.trim() !== "");
+                    for (const line of lines) {
+                        const message = line.replace(/^data: /, "");
+                        if (message.includes("[DONE]")) return;
                         try {
-                            const chunk = data.choices[0].delta?.content;
+                            const parsed = JSON.parse(message);
+                            const chunk = parsed.choices[0].delta?.content;
                             if (chunk) {
-                                console.log(colors.green("Bot: ") + chunk);
-                                
-                                // Update history with assistant response
-                                chatHistory.push(["assistant", chunk]);
+                                if (isFirstChunk) {  // Add this block
+                                    process.stdout.write(colors.green("Bot: " + chunk));
+                                    isFirstChunk = false;
+                                } else {
+                                    process.stdout.write(colors.green(chunk));
+                                }
                             }
                         } catch (error) {
-                            console.log(`Error with JSON.parse and ${payload}.\n${error}`);
+                            console.error("Could not JSON parse stream message", message, error);
                         }
                     }
-                }
-            });
+                });
 
-            stream.on('end', () => {
-                setTimeout(() => {
-                    console.log('\nStream done');
-                }, 10);
-            });
+                stream.on("end", () => {
+                    console.log("");
+                    resolve();
+                });
 
-            stream.on('error', (err) => {
-                console.error(colors.red(err));
-            });
-
-        } catch (error) {
-            console.error(colors.red(error));
-        }
+                stream.on("error", (err) => {
+                    console.error(colors.red(err));
+                    reject(err);
+                });
+            } catch (error) {
+                console.error(colors.red(error));
+                reject(error);
+            }
+        });
     }
 }
 
