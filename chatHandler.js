db26a1336f8b0334f41b637db5d179c0897f8d29
MilleniumDawn <122926088+MilleniumDawn@users.noreply.github.com>
2023-10-29 15:51:51 -0400

    more config, centralise

--- chatHandler.js
+++ chatHandler.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import { config } from './config.js';
-import { UserHistoryManager } from './userHistoryManager.js';
+import axios from "axios";
+import { config } from "./config.js";
+import { UserHistoryManager } from "./userHistoryManager.js";
 
 const userHistoryManager = new UserHistoryManager();
 
 export async function handleUserMessage(socket, messageHistory, msg) {
     let isFirstChunk = true;
-    let accumulatedResponse = '';
+    let accumulatedResponse = "";
 
     // Ensure the message history is within the token limit
     userHistoryManager.ensureTokenLimit(socket.id);
@@ -18,14 +18,21 @@ export async function handleUserMessage(socket, messageHistory, msg) {
             responseType: "stream",
         });
 
-        const response = await axiosInstance.post("/chat/completions", {
-            messages: messageHistory,
-            stop: ["<.>"],
-            temperature: 0.7,
-            max_tokens: config.MAX_TOKENS,
-            stream: true,
-            repetition_penalty: 1.1,
-        });
+        const response = await axiosInstance.post(
+            `${config.apiBaseUrl}${config.API_ENDPOINT}`, 
+            {
+                messages: messageHistory,
+                stop: config.STOP_SEQUENCE,
+                temperature: config.TEMPERATURE,
+                max_tokens: config.MAX_TOKENS,
+                stream: config.STREAM,
+                repetition_penalty: config.REPETITION_PENALTY,
+                top_p: config.TOP_P,
+                top_k: config.TOP_K,
+                frequency_penalty: config.FREQUENCY_PENALTY,
+                presence_penalty: config.PRESENCE_PENALTY,
+            }
+        );
 
         const stream = response.data;
 
@@ -41,13 +48,13 @@ export async function handleUserMessage(socket, messageHistory, msg) {
                     const parsed = JSON.parse(message);
                     const chunk = parsed.choices[0].delta?.content;
                     if (chunk) {
-                        accumulatedResponse += chunk;  // Accumulate chunks for message history
+                        accumulatedResponse += chunk; // Accumulate chunks for message history
 
                         if (isFirstChunk) {
-                            socket.emit('bot message', 'Bot: ' + chunk);
+                            socket.emit("bot message", "Bot: " + chunk);
                             isFirstChunk = false;
                         } else {
-                            socket.emit('bot message', chunk);
+                            socket.emit("bot message", chunk);
                         }
                     }
                 } catch (error) {
@@ -57,21 +64,20 @@ export async function handleUserMessage(socket, messageHistory, msg) {
         });
 
         stream.on("end", () => {
-            console.log('Stream done');
+            console.log("Stream done");
             // Update message history with the accumulated bot message
             messageHistory.push({
                 role: "assistant",
-                content: accumulatedResponse
+                content: accumulatedResponse,
             });
         });
 
         stream.on("error", (err) => {
             console.error(err);
-            socket.emit('error message', 'An error occurred while processing your request.');
+            socket.emit("error message", "An error occurred while processing your request.");
         });
     } catch (error) {
         console.error(error);
-        socket.emit('error message', 'An error occurred while processing your request.');
+        socket.emit("error message", "An error occurred while processing your request.");
     }
 }
-
--- config.js
+++ config.js
@@ -3,4 +3,13 @@ export const config = {
     apiBaseUrl: process.env.API_BASE_URL || "http://localhost:1234/v1",
     port: process.env.PORT || 3002,
     MAX_TOKENS: process.env.MAX_TOKENS || 4096,
+    API_ENDPOINT: "/chat/completions",
+    STOP_SEQUENCE: ["<.>"],
+    TEMPERATURE: 0.4,
+    STREAM: true,
+    REPETITION_PENALTY: 1.1,
+    TOP_P: 0.9,
+    TOP_K: 20,
+    FREQUENCY_PENALTY: 0.5,
+    PRESENCE_PENALTY: 0.5,
 };
