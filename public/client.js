2a6942d2e7a13b8a1fa3e0d1e68aacfc1cb24de9
MilleniumDawn <122926088+MilleniumDawn@users.noreply.github.com>
2023-10-29 15:22:48 -0400

    fix css

--- public/client.js
+++ public/client.js
@@ -6,13 +6,12 @@ const socket = io();
 let botMessageDiv; // Declare a variable to hold the current bot message div
 
 async function fetchConfig() {
-    const response = await fetch('/config');
+    const response = await fetch("/config");
     const config = await response.json();
-    document.querySelector('.modelName').textContent = `This AI is running locally using the model "${config.modelName}" on LM Studio.`;
+    document.querySelector(".modelName").textContent = `This AI is running locally using the model "${config.modelName}" on LM Studio.`;
 }
 
 fetchConfig();
-
 
 function sendMessage() {
     const message = document.getElementById("input").value;
